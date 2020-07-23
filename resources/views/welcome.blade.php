d955d8a99978c834523cf7486f46a6c500dfa560
jjrohrer <jj.bitbucket@rohrer.org>
2020-07-23 08:11:20 -0400

    moved demo to package

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,47 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
+    <hr>
+    <h2>TallAndSassy_ThemeHints Demo</h2>
+@include('eleganttechnologies::TallAndSassy_ThemeHints_demo')
 
-        <div>
-        This would be a link to a new page
-                <a href="http://google.com" class="{{ config('tallandsassy_themehints.link_to_classes') }}" >Google</a>
-            --or, if it worked, like this --...
-            @component('eleganttechnologies::components.TextLinkToPage',['href'=>'http://www.google.com', 'text'=>"Why not worky"])
-        </div>
-        <hr>
 
-        <div>
-        This would be a link to a new tab: <a href="http://google.com"  target="_blank" class="{{ config('tallandsassy_themehints.new_tab_classes') }}" style="{{ config('tallandsassy_themehints.new_tab_style') }}" >Google {!! config('tallandsassy_themehints.new_tab_icon_html') !!}</a>
-        </div>
-        <hr>
-        --- Centered Buttons ---
-        <p>
-        <div class="flex items-center justify-center">
-            <div class="flex flex-col justify-around">
-                <div class="space-y-6">
-                    @component('eleganttechnologies::components.buttons.standard_bottom',['type'=>'Primary'])
-                        @slot('text')
-                            Button:eleganttechnologies::components.buttons.standard_bottom
-                        @endslot
-                        @slot('click')
-                            alert('hi')
-                        @endslot
-                    @endcomponent
-                    <hr>
-                    @component('eleganttechnologies::components.buttons.standard_bottom',['type'=>'Secondary'])
-                        @slot('text')
-                            A Secondary button
-                        @endslot
-                        @slot('click')
-                            close()
-                        @endslot
-                    @endcomponent
-                </div>
-            </div>
-        </div>
-    </div>
-
-    @include('eleganttechnologies::embedded_stylesheet')
 
 @endsection
